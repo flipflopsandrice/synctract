a141d0f5f36e004c733084aa7df34b36f4f9ab1b
Erik <erik@thorongil.com>
2015-01-21 15:16:26 +0100

    first version

---
+++ README.md
@@ -0,0 +1,34 @@
+# Synctract
+Scripts to sync a remote folder and automatically extract archives.
+
+*Note: This was developed for use on Synology devices.*
+
+### Step 1: Exchange ssh pubkeys
+On both your **remote** (user that you will ssh into) and **local** (user that will be running the script) machine (use default settings):
+```
+user1@local:~$mkdir ~/.ssh &&  touch ~/.ssh/authorized_keys && ssh-keygen
+```
+Upload pubkey to **remote**:
+```
+user1@local:~$ scp user2@remote:/home/user2/.ssh/id_rsa.pub /tmp/tmp_rsa_key && cat /tmp/tmp_rsa_key >> ~/.ssh/authorized_keys && rm /tmp/tmp_rsa_key
+```
+Download pubkey to **local**:
+```
+user1@local:~$ scp ~/.ssh/id_rsa.pub user2@remote:/tmp/tmp_rsa_key && ssh -X user2@remote 'cat /tmp/tmp_rsa_key >> ~/.ssh/authorized_keys && rm /tmp/tmp_rsa_key'
+```
+### Step 2: Configure
+```
+user1@local:~$ cd <path-to-synctract> && cp config-sample.cfg config.cfg
+```
+Editing the sample config to match your environments.
+
+### Step 3: Cronjob
+
+Use the system's crontab (or if available, edit your user's crontab through *`crontab -e`*):
+```
+vi /etc/crontab
+```
+And add the script to run whenever you like (**here it's every minute**):
+```
+*      *       *       *       *       root    /<path-to-script>/synctract.sh
+```
---
+++ synctract.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+CONFIGFILE="$SCRIPTPATH/"config.cfg
+
+### Import configuration
+if [ ! -f "$CONFIGFILE" ]
+then
+    echo No configuration file found, exiting.
+    exit;
+fi
+
+eval $(sed '/:/!d;/^ *#/d;s/:/ /;' < "$CONFIGFILE" | while read -r key val
+do
+    str="$key='$val'"
+    echo "$str"
+done)
+
+IGNORELIST="$local_tmp"/synctract.ignore
+
+### Check for lock (= running instance)
+if [ -f "$local_tmp"/synctract.lock ]
+then
+    echo "Lock found, exiting."
+    exit;
+fi
+touch "$local_tmp"/synctract.lock
+touch "$IGNORELIST"
+
+### Rsync data
+rsync -rlptgovzEhv -avzq --bwlimit=1536 --specials --ignore-errors --exclude-from "$IGNORELIST" "$remote_user"@"$remote_hostname":"$remote_folder"/ "$local_destination"
+find "$local_destination" | sed "s#$local_destination/##g" >> "$IGNORELIST"
+
+### Find available archives
+RARS=`find "$local_destination" | grep .rar | sed "s#$local_destination/##g" | grep -v "@eaDir"`
+for RAR in `echo "$RARS"`
+do
+    CONTENTS=`unrar l "$local_destination"/"$RAR" | grep -o ':[0-9]\{2\}\ \ \(.*\)$' | awk '{printf $2 "\n"}'`
+    MISSING=""
+    for CONTENT in `echo "$CONTENTS"`
+    do
+        FOUND=`find "$local_destination" | grep "$CONTENT$"`
+        if [ -z "$FOUND" ]
+        then
+            MISSING=1;
+            echo ""
+        fi
+    done
+
+    if [ ! -z "${MISSING}" ]
+    then
+        unrar x "$local_destination"/"$RAR" `dirname "$local_destination"/"$RAR"` > /dev/null 2>&1
+    fi
+done
+
+### Release lock
+rm "$local_tmp"/synctract.lock;
